49371e0b5e6afac4128c1750657ffa4b40f4bba2
sanotje <sanae.akh@hotmail.com>
2022-05-16 12:11:05 +0200

    add all files

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+/*const port = 3000;*/
 
 /*****************************************************
  * Middleware
@@ -73,6 +73,6 @@ app.get("/myfavorites", (req, res) => {
 /*****************************************************
  * Start webserver
  ****************************************************/
-app.listen(port, () => {
-  console.log(`Webserver running on port localhost:${port}`);
+app.listen(process.env.PORT, () => {
+  console.log(`App listening on port ${process.env.PORT}`);
 });
