57570b97915adc0e6ac1475436b95dd4949753ff
sanotje <sanae.akh@hotmail.com>
2022-05-16 11:47:10 +0200

    add all files

--- server.js
+++ server.js
@@ -8,16 +8,19 @@ const port = 3000;
 app.use(express.static("public"));
 // const profiles = require("./data")
 
+
+
 const profiles = [
     {
-      name: "Peter",
-      foto: "",
+      foto: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.geertvandeven.com%2Fprofielfoto%2F&psig=AOvVaw3a1qiqZwGA9Pri06kh2ZAG&ust=1652699284033000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCIis3uCu4fcCFQAAAAAdAAAAABAD",
+      name: "Petra",
+      quote: "A nanny is a little bit parent, a little bit teacher and a little bit best friend!",
       age: "32",
       plaats: "Utrecht"
     },
     {
       name: "Sanae",
-      foto: "",
+      foto: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.profielfotoshop.nl%2F&psig=AOvVaw3a1qiqZwGA9Pri06kh2ZAG&ust=1652699284033000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCIis3uCu4fcCFQAAAAAdAAAAABAf",
       age: "20",
       plaats: "Haarlem"
     },
@@ -50,6 +53,18 @@ app.get("/", (req, res) => {
       profiles: profiles
   });
 });
+
+app.get("/index", (req, res) => {
+    res.render("pages/index.ejs", {
+        profiles: profiles
+    });
+  });
+
+app.get("/myfavorites", (req, res) => {
+    res.render("pages/myfavorites.ejs", {
+        profiles: profiles
+    });
+  });
 
 /*****************************************************
  * If no routes give response, show 404 Page
